e59c2895e1926c219a8eff59275192338aefb463
Tinu <tinuan96@gmail.com>
2026-07-03 11:08:24 +0100

    Auto-commit: 2026-07-03T10:08:23.522Z

--- android/app/src/main/java/com/keralagrocery/app/MainActivity.java
+++ android/app/src/main/java/com/keralagrocery/app/MainActivity.java
@@ -1,5 +1,23 @@
 package com.keralagrocery.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Enable popups for payment gateways like Google Pay
+        WebView webView = getBridge().getWebView();
+        if (webView != null) {
+            webView.getSettings().setSupportMultipleWindows(true);
+            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        }
+    }
+}
