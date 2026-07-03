fd55571303c16664ca6ec07c364358dfdc758924
Tinu <tinuan96@gmail.com>
2026-07-03 08:46:53 +0100

    Auto-commit: 2026-07-03T07:46:52.193Z

--- scripts/git-sync.js
+++ scripts/git-sync.js
@@ -19,6 +19,8 @@ const commitMessage = `Auto-commit: ${timestamp}`;
 
 console.log('Starting git sync...');
 
+run('git fetch origin');
+run('git pull origin main --rebase');
 run('git add .');
 
 // Check if there are changes to commit
