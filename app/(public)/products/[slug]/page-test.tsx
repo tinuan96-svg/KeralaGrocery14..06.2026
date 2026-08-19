fb65179047f1b9a8f3fcca695f6bb373e51a7bc7
Tinu <tinuan96@gmail.com>
2026-08-19 15:33:22 +0100

    Auto-commit: 2026-08-19T14:33:21.280Z

--- app/(public)/products/[slug]/page-test.tsx
+++
@@ -1,7 +0,0 @@
-export const dynamicParams = false;
-export async function generateStaticParams() {
-  return [{ slug: 'test' }];
-}
-export default function TestPage() {
-  return <div>Test</div>;
-}
